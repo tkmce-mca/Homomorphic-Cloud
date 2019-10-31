f2610067de47a103636565d97d05028fa8c808d0
aksawilson <48819913+aksawilson@users.noreply.github.com>
2019-10-31 12:23:40 -0700

    Add files via upload

---
+++ AWSClientFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Amazon.S3;
+
+namespace Amazon
+{
+    internal class AWSClientFactory
+    {
+        internal static IAmazonS3 CreateAmazonS3Client(object uSEast)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
